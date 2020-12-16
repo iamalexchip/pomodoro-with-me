abb461bbcded7e9d98df729df5c737006d045dc1
Alex <iamalexchip@gmail.com>
2020-12-16 10:44:58 +0200

    fix:b: Static react files path

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,12 +17,12 @@ const main = async () => {
     apollo.applyMiddleware({app});
 
     // Serve static files from the React app
-    app.use(Express.static(path.join(__dirname, '../../build')));
+    app.use(Express.static(path.join(__dirname, '../../frontend/build')));
     
     // The "catchall" handler: for any request that doesn't
     // match one above, send back React's index.html file.
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../build/index.html'));
+        res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
     });
 
     app.listen({ port }, () =>
